1703fa3de9b3cbbb6d617bf2a85509b4cbf39427
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-12-26 00:21:01 +0700

    create serialize login and address

--- user/serializers.py
+++ user/serializers.py
@@ -1,9 +1,19 @@
+from typing import Any, Dict
 from rest_framework import serializers
-from .models import UserProfiles
+from .models import *
 from rolepermissions.roles import assign_role
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = "__all__"
 
 
 class UserProfilesSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = UserProfiles
         fields = "__all__"
@@ -31,3 +41,20 @@ class RegisterUserSerializer(serializers.ModelSerializer):
         assign_role(user, 'user')
 
         return user
+
+
+class LoginSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        return token
+
+    def validate(self, attrs):
+        user = UserProfiles.objects.filter(
+            email=attrs.get('email', '')).first()
+        if user is None:
+            raise serializers.ValidationError("User not found.")
+        if not user.is_email_verified:
+            raise serializers.ValidationError("User is not verified.")
+
+        return super(LoginSerializer, self).validate(attrs)
