da78258da3a449fe86e14686937222b4cac0a88f
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-12-25 20:17:55 +0700

    setting vault secreate key

--- auth/settings.py
+++ auth/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "auth.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": SECRETE_KEY_SERVICE['data']['data']['ENGINE'],
+        "NAME": SECRETE_KEY_SERVICE['data']['data']['DB_NAME'],
+        "USER": SECRETE_DB_SERVICE['data']['username'],
+        "PASSWORD": SECRETE_DB_SERVICE['data']['password'],
+        "HOST": SECRETE_KEY_SERVICE['data']['data']['DB_HOST'],
+        "PORT": SECRETE_KEY_SERVICE['data']['data']['DB_PORT'],
     }
 }
 
--- auth/vault.py
+++ auth/vault.py
@@ -6,10 +6,21 @@ import sys
 
 # Authentication
 client = hvac.Client(
-    url=env('VAULT_URI'),
-    token=env('VAULT_TOKEN'),
+    url=env('VAULT_URI')
 )
+# print(client.token)
+
+# print("is_sealed ", client.sys.is_sealed())
+# print(client.is_authenticated())
+
 
 # Reading a secret
 SECRETE_KEY_SERVICE = client.secrets.kv.read_secret_version(
     path=env('VAULT_PATH'))
+
+SECRETE_DB_SERVICE = client.secrets.database.get_static_credentials(
+    name=env('VAULT_DB_ROLE'),
+    mount_point=env('VAULT_DB_PATH')
+)
+# print(SECRETE_DB_SERVICE['data']['password'],
+#       SECRETE_DB_SERVICE['data']['username'])
