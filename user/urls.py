d4f6494b8e15a4f911fd344ca11e3f96f001bad0
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-12-26 00:22:33 +0700

    add route autorization and address

--- user/urls.py
+++ user/urls.py
@@ -7,9 +7,10 @@ from rest_framework_simplejwt.views import (
 )
 from .views import *
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', LoginAPIView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('register/', RegisterAPIView.as_view(), name='register'),
-
+    path('profile/', UserProfilesAPIView.as_view(), name='profile'),
+    path('address/', AddressAPIView.as_view(), name='address'),
 ]
