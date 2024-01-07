5398992566e7a87fc93fac55cdd27e1288d67c83
ratchaphon1412 <ratchaphon.h111@gmail.com>
2024-01-08 01:31:37 +0700

    fix error deploy

--- server.sh
+++ server.sh
@@ -6,6 +6,3 @@ poetry run python3 manage.py collectstatic --noinput
 echo "-----------Apply migration--------- "
 poetry run python3 manage.py makemigrations
 poetry run python3 manage.py migrate
-
-echo "-----------Run gunicorn--------- "
-poetry run gunicorn -b :80 auth.wsgi:application
