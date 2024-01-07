0e045b79317ecb4673a8c7a6f9e00bfc6f48bf46
ratchaphon1412 <ratchaphon.h111@gmail.com>
2024-01-08 01:41:17 +0700

    fix again

--- server.sh
+++ server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "----- Collect static files ------ " 
-poetry run python3 manage.py collectstatic --noinput
-
 echo "-----------Apply migration--------- "
 poetry run python3 manage.py makemigrations
 poetry run python3 manage.py migrate
