43f1db1ea09116e2e6826a73b3c2cb4f00055818
ratchaphon1412 <ratchaphon.h111@gmail.com>
2024-01-30 01:15:55 +0700

    edit receiveer function kafka update token

--- infrastructure/event/updateUser.py
+++ infrastructure/event/updateUser.py
@@ -1,7 +1,22 @@
+from user.models import UserProfiles
+
 
 def updateVerifyEmail(data):
+
     print("verify email")
+    # Use the data_dict dictionary as needed
 
 
 def updateOmiseKey(data):
-    print("update omise key")
+
+    print(type(data), data["email"])
+
+    user = UserProfiles.objects.filter(email=data["email"]).update(
+        customer_omise_id=data["token"])
+
+    if user:
+        print("update omise success")
+    else:
+        print("update omise fail")
+
+    # Use the data_dict dictionary as needed
